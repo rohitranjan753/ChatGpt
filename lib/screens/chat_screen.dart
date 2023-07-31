fe68fe123b4d8a19684429a8910b0e4a71921f7c
Rohit Ranjan <rohitgns54321@gmail.com>
2023-07-31 09:33:35 +0530

    Chatscreen spinkit

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,5 +1,6 @@
 import 'package:chatgptbot/services/assets_manager.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChatScreen extends StatefulWidget {
   const ChatScreen({Key? key}) : super(key: key);
@@ -9,10 +10,32 @@ class ChatScreen extends StatefulWidget {
 }
 
 class _ChatScreenState extends State<ChatScreen> {
+  final bool _isTyping = true;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Image.asset(AssetsManager.openaiLogo),),
+      appBar: AppBar(
+        title: const Text("ChatGPT"),
+        elevation: 2,
+        leading: Image.asset(AssetsManager.openaiLogo),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            Flexible(
+              child: ListView.builder(itemCount: 6,itemBuilder: (context,index){
+                return const Text("hELLO");
+              },),
+            ),
+            if(_isTyping) ...[
+              const SpinKitThreeBounce(
+                color: Colors.white,
+                size: 18,
+              )
+            ]
+          ],
+        ),
+      )
     );
   }
 }
